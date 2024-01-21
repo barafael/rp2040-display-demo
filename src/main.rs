c530094ede74f3301b43635c6a9a548ee1737ced
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2024-01-21 12:26:22 +0100

    Make multicore
    blinky keeps going no matter what happens on the other core, even if the other core hangs

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@
 
 use crate::{formatter::Formatter, progressbar::ProgressBar};
 use core::fmt::Write;
-use embassy_executor::Spawner;
+use defmt::info;
+use embassy_executor::Executor;
 use embassy_rp::{
     bind_interrupts,
     config::Config,
     gpio::{Level, Output},
-    i2c,
+    i2c::{self, I2c},
+    multicore::{spawn_core1, Stack},
     peripherals::{I2C0, PIN_25},
     watchdog::Watchdog,
 };
@@ -23,10 +25,17 @@ use embedded_graphics::{
     text::{Alignment, Baseline, Text},
 };
 
+pub const CORE1_STACK_SIZE: usize = 65_536;
+
+static CORE1_STACK: StaticCell<Stack<CORE1_STACK_SIZE>> = StaticCell::new();
+static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
+static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
+
 use display_interface_i2c::I2CInterface;
 use ssd1309::{
     displayrotation::DisplayRotation, mode::GraphicsMode, prelude::DisplaySize, Builder,
 };
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
 mod formatter;
@@ -39,8 +48,12 @@ bind_interrupts!(struct Irqs {
     I2C0_IRQ => i2c::InterruptHandler<I2C0>;
 });
 
-#[embassy_executor::main]
-async fn main(_s: Spawner) {
+const INTRO_STYLE: MonoTextStyle<'_, BinaryColor> =
+    MonoTextStyle::new(&INTRO_FONT, BinaryColor::On);
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    info!("Hi display demo");
     let p = embassy_rp::init(Config::default());
 
     let mut i2c0_config = i2c::Config::default();
@@ -51,8 +64,8 @@ async fn main(_s: Spawner) {
 
     let mut oled_reset = Output::new(p.PIN_4, Level::Low);
 
-    let mut led = Output::new(p.PIN_25, Level::Low);
-    let mut watchdog = Watchdog::new(p.WATCHDOG);
+    let led = Output::new(p.PIN_25, Level::Low);
+    let watchdog = Watchdog::new(p.WATCHDOG);
 
     let i2c0_bus = i2c::I2c::new_async(p.I2C0, i2c0_scl, i2c0_sda, Irqs, i2c0_config);
     let display_interface = I2CInterface::new(i2c0_bus, 0x3C, 0x40);
@@ -71,9 +84,20 @@ async fn main(_s: Spawner) {
     display.clear();
     display.flush().unwrap();
 
-    const INTRO_STYLE: MonoTextStyle<'_, BinaryColor> =
-        MonoTextStyle::new(&INTRO_FONT, BinaryColor::On);
+    spawn_core1(p.CORE1, CORE1_STACK.init(Stack::new()), move || {
+        let executor1 = EXECUTOR1.init(Executor::new());
+        executor1.run(|spawner| spawner.spawn(progress(display, watchdog)).unwrap())
+    });
 
+    let executor0 = EXECUTOR0.init(Executor::new());
+    executor0.run(|spawner| spawner.spawn(blinky(led)).unwrap());
+}
+
+#[embassy_executor::task]
+async fn progress(
+    mut display: GraphicsMode<I2CInterface<I2c<'static, I2C0, i2c::Async>>>,
+    mut watchdog: Watchdog,
+) -> ! {
     Text::with_alignment(
         "Hi there",
         Point::new(64, 22),
@@ -100,7 +124,7 @@ async fn main(_s: Spawner) {
         display.clear();
         let progress = (index % 100) as f32 * (1.0 / 100.0);
         if let Err(e) = pb.draw(progress, &mut display) {
-            on_bus_error(&mut watchdog, &mut led).await;
+            on_bus_error(&mut watchdog).await;
         }
 
         let mut buf = Formatter::<96>::new();
@@ -109,21 +133,26 @@ async fn main(_s: Spawner) {
             Text::with_baseline(buf.as_str(), Point::new(8, 8), INTRO_STYLE, Baseline::Top)
                 .draw(&mut display)
         {
-            on_bus_error(&mut watchdog, &mut led).await;
+            on_bus_error(&mut watchdog).await;
         }
 
         if let Err(e) = display.flush() {
-            on_bus_error(&mut watchdog, &mut led).await;
+            on_bus_error(&mut watchdog).await;
         }
 
         index += 1;
     }
 }
 
-async fn on_bus_error(wdg: &mut Watchdog, led: &mut Output<'static, PIN_25>) -> ! {
-    led.set_high();
-    Timer::after_millis(100).await;
-    led.set_low();
+async fn on_bus_error(wdg: &mut Watchdog) -> ! {
     wdg.trigger_reset();
     unreachable!("Watchdog triggered");
+}
+
+#[embassy_executor::task]
+async fn blinky(mut led: Output<'static, PIN_25>) -> ! {
+    loop {
+        Timer::after_millis(400).await;
+        led.toggle();
+    }
 }
