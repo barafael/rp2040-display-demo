70127653acb2f379172e41bc7d5acef0497e16d0
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2024-02-06 22:27:58 +0100

    Simplify, just draws rectangles

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,20 @@
 #![forbid(unsafe_code)]
 #![allow(rustdoc::bare_urls)]
 
-use crate::{formatter::Formatter, progressbar::ProgressBar};
-use core::fmt::Write;
-use defmt::info;
+use crate::progressbar::ProgressBar;
+use cortex_m::prelude::_embedded_hal_blocking_delay_DelayMs;
+use defmt::{info, trace};
 use embassy_executor::Executor;
 use embassy_rp::{
     bind_interrupts,
     config::Config,
-    gpio::{Level, Output},
+    gpio::{Input, Level, Output, Pull},
     i2c::{self, I2c},
     multicore::{spawn_core1, Stack},
-    peripherals::{I2C0, PIN_25},
+    peripherals::{I2C0, PIN_25, PIN_5},
     watchdog::Watchdog,
 };
 use embassy_time::Timer;
-use embedded_graphics::{
-    mono_font::{ascii::FONT_7X13 as INTRO_FONT, MonoTextStyle},
-    pixelcolor::BinaryColor,
-    prelude::*,
-    text::{Alignment, Baseline, Text},
-};
 
 pub const CORE1_STACK_SIZE: usize = 65_536;
 
@@ -38,7 +32,6 @@ use ssd1309::{
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
-mod formatter;
 mod progressbar;
 
 const I2C0_FREQUENCY_HZ: u32 = 400_000;
@@ -47,9 +40,6 @@ const I2C0_FREQUENCY_HZ: u32 = 400_000;
 bind_interrupts!(struct Irqs {
     I2C0_IRQ => i2c::InterruptHandler<I2C0>;
 });
-
-const INTRO_STYLE: MonoTextStyle<'_, BinaryColor> =
-    MonoTextStyle::new(&INTRO_FONT, BinaryColor::On);
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -62,11 +52,14 @@ fn main() -> ! {
     let i2c0_sda = p.PIN_0;
     let i2c0_scl = p.PIN_1;
 
+    let button = Input::new(p.PIN_5, Pull::None);
+
     let mut oled_reset = Output::new(p.PIN_4, Level::Low);
 
     let led = Output::new(p.PIN_25, Level::Low);
     let watchdog = Watchdog::new(p.WATCHDOG);
 
+    let mut delay = embassy_time::Delay;
     let i2c0_bus = i2c::I2c::new_async(p.I2C0, i2c0_scl, i2c0_sda, Irqs, i2c0_config);
     let display_interface = I2CInterface::new(i2c0_bus, 0x3C, 0x40);
     let mut display: GraphicsMode<_> = Builder::new()
@@ -74,8 +67,15 @@ fn main() -> ! {
         .with_rotation(DisplayRotation::Rotate0)
         .connect(display_interface)
         .into();
-
-    let mut delay = embassy_time::Delay;
+    display
+        .reset(&mut oled_reset, &mut embassy_time::Delay)
+        .unwrap();
+    oled_reset.set_high();
+    oled_reset.set_low();
+    delay.delay_ms(10u32);
+    oled_reset.set_high();
+    display.init().unwrap();
+    display.flush().unwrap();
 
     display.reset(&mut oled_reset, &mut delay).unwrap();
 
@@ -86,7 +86,7 @@ fn main() -> ! {
 
     spawn_core1(p.CORE1, CORE1_STACK.init(Stack::new()), move || {
         let executor1 = EXECUTOR1.init(Executor::new());
-        executor1.run(|spawner| spawner.spawn(progress(display, watchdog)).unwrap())
+        executor1.run(|spawner| spawner.spawn(progress(display, button, watchdog)).unwrap())
     });
 
     let executor0 = EXECUTOR0.init(Executor::new());
@@ -96,55 +96,32 @@ fn main() -> ! {
 #[embassy_executor::task]
 async fn progress(
     mut display: GraphicsMode<I2CInterface<I2c<'static, I2C0, i2c::Async>>>,
+    button: Input<'static, PIN_5>,
     mut watchdog: Watchdog,
 ) -> ! {
-    Text::with_alignment(
-        "Hi there",
-        Point::new(64, 22),
-        INTRO_STYLE,
-        Alignment::Center,
-    )
-    .draw(&mut display)
-    .unwrap();
-    Text::with_alignment(
-        "let's test display",
-        Point::new(64, 52),
-        INTRO_STYLE,
-        Alignment::Center,
-    )
-    .draw(&mut display)
-    .unwrap();
-    display.flush().unwrap();
-
-    Timer::after_millis(400).await;
-
     let mut pb = ProgressBar::new(10, 35, 108, 10);
     let mut index = 0u64;
     loop {
+        while button.is_low() {
+            continue;
+        }
+        while button.is_high() {
+            continue;
+        }
         display.clear();
         let progress = (index % 100) as f32 * (1.0 / 100.0);
-        if let Err(e) = pb.draw(progress, &mut display) {
-            on_bus_error(&mut watchdog).await;
+        trace!("loop {}", progress);
+        if let Err(_e) = pb.draw(progress, &mut display) {
+            on_bus_error(&mut watchdog);
         }
-
-        let mut buf = Formatter::<96>::new();
-        write!(buf, "i: {}", index).unwrap();
-        if let Err(e) =
-            Text::with_baseline(buf.as_str(), Point::new(8, 8), INTRO_STYLE, Baseline::Top)
-                .draw(&mut display)
-        {
-            on_bus_error(&mut watchdog).await;
+        if let Err(_e) = display.flush() {
+            on_bus_error(&mut watchdog);
         }
-
-        if let Err(e) = display.flush() {
-            on_bus_error(&mut watchdog).await;
-        }
-
         index += 1;
     }
 }
 
-async fn on_bus_error(wdg: &mut Watchdog) -> ! {
+fn on_bus_error(wdg: &mut Watchdog) -> ! {
     wdg.trigger_reset();
     unreachable!("Watchdog triggered");
 }
--- src/progressbar.rs
+++ src/progressbar.rs
@@ -11,12 +11,6 @@ pub struct ProgressBar {
     width: u32,
 }
 
-const STYLE: PrimitiveStyle<BinaryColor> = PrimitiveStyleBuilder::new()
-    .stroke_color(BinaryColor::On)
-    .stroke_width(1)
-    .fill_color(BinaryColor::Off)
-    .build();
-
 const FILL_STYLE: PrimitiveStyle<BinaryColor> = PrimitiveStyleBuilder::new()
     .stroke_color(BinaryColor::On)
     .stroke_width(1)
@@ -38,12 +32,6 @@ impl ProgressBar {
         D: DrawTarget<Color = BinaryColor>,
         D::Error: core::fmt::Debug,
     {
-        Rectangle::new(
-            Point::new(self.x as i32, self.y as i32),
-            Size::new(self.length, self.width),
-        )
-        .into_styled(STYLE)
-        .draw(display)?;
         let fill = (self.length as f32 * ratio) as u32;
         Rectangle::new(
             Point::new(self.x as i32, self.y as i32),
